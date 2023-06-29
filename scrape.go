7c55c5b3289d3419cbb4d9ff97fa2e66c1078cc2
alexthegreat194 <alexander.gentry@aol.com>
2023-06-29 16:44:09 -0700

    save to json

--- scrape.go
+++ scrape.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
 type Card struct {
-	productName string
-	rarity string
-	number string
-	marketPrice string
-	listedMedian string
+	ProductName string `json:"productName"`
+	Rarity string `json:"rarity"`
+	Number string `json:"number"`
+	MarketPrice string `json:"marketPrice"`
+	ListedMedian string `json:"listedMedian"`
 }
+
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -25,13 +29,13 @@ func main() {
 	c.OnHTML(".priceGuideTable", func(e *colly.HTMLElement) {
 
 		e.ForEach(".priceGuideTable tbody tr", func(i int, e *colly.HTMLElement) {
-			fmt.Println("Line: ", i)
+			// fmt.Println("Line: ", i)
 			var foundCard Card
-			foundCard.productName = e.ChildText(".product .cellWrapper .productDetail a")
-			foundCard.rarity = e.ChildText(".rarity .cellWrapper")
-			foundCard.number = e.ChildText(".number .cellWrapper")
-			foundCard.marketPrice = e.ChildText(".marketPrice .cellWrapper")
-			foundCard.listedMedian = e.ChildText(".medianPrice .cellWrapper")
+			foundCard.ProductName = e.ChildText(".product .cellWrapper .productDetail a")
+			foundCard.Rarity = e.ChildText(".rarity .cellWrapper")
+			foundCard.Number = e.ChildText(".number .cellWrapper")
+			foundCard.MarketPrice = e.ChildText(".marketPrice .cellWrapper")
+			foundCard.ListedMedian = e.ChildText(".medianPrice .cellWrapper")
 			// fmt.Println("Card Info: ", foundCard)
 			foundCardList = append(foundCardList, foundCard)
 		})
@@ -53,6 +57,36 @@ func main() {
         fmt.Println("Finished", r.Request.URL)
 
 		fmt.Println("FoundCardList: ", foundCardList)
+		jsonList := []string{}
+		for _, v := range foundCardList {
+			data, err := json.Marshal(v)
+			if (err != nil) {
+				panic(err)
+			}
+			// fmt.Println(v, string(data))
+			jsonList = append(jsonList, string(data))
+		}
+
+		startString := "{\"url\":\"https://shop.tcgplayer.com/price-guide/pokemon\", \"data\":["
+		endString := "]}"
+
+		f, err := os.Create("output.json")
+		if (err != nil) {
+			panic(err)
+		}
+
+		fs := bufio.NewWriter(f)
+		fs.WriteString(startString)
+		for i, v := range jsonList {
+			b, err := fs.WriteString(v)
+			if (err != nil) {
+				panic(err)
+			}
+			fmt.Printf("Added %v : %v : %v to File\n", i, v, b)
+		}
+		fs.WriteString(endString)
+
+		fmt.Printf("Added to File\n")
     })
 
 	// Start scraping on https://hackerspaces.org
